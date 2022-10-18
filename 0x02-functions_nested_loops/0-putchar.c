07dc1d412cc706b9f7b5c7723dadb83e6581a430
Elizabeth K. G-S <81371428+lizodus@users.noreply.github.com>
2022-10-19 01:14:32 +0400

    Betty Again

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
- * _putchar - writes the character to stdout
- *  main - function that prints _putchar
- * Return: On success 0.
+ * main - function that prints _putchar
+ * 
+ * Return: always return 0
+ * 
  */
 int main(void)
 {
-    _putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
