a4a204def49eb0d983d2243db685a04678c0a8cb
Elizabeth K. G-S <81371428+lizodus@users.noreply.github.com>
2022-10-18 17:37:47 +0400

    nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 0.
+ */
+int main(void)
+{
+char *sh = "_putchar";
+while (*sh)
+{
+_putchar(*sh);
+sh++;
+}
+_putchar('\n');
+return (0);
+}
