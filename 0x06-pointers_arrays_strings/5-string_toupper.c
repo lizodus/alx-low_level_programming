8c588a400ea47b3008629e2e7fb611c443db7e4c
Elizabeth K. G-S <81371428+lizodus@users.noreply.github.com>
2022-10-27 20:40:14 +0400

    main update

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,7 +3,7 @@ include "main.h"
 /**
  * string_toupper - Changes all lowercase letters
  *                  of a string to uppercase.
- * @str: The string to be changed.
+ * @s: The string to be changed.
  *
  * Return: A pointer to the changed string.
  */
