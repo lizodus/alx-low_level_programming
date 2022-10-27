27d3b20160111a47b9ad91e4cd22ea5cf6597064
Elizabeth K. G-S <81371428+lizodus@users.noreply.github.com>
2022-10-28 00:07:26 +0400

    toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,14 +9,17 @@ include "main.h"
  */
 char *string_toupper(char *s)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i])
 	{
-		if (s[i] >= 'a' && s[i] <= 'z')
-		{
-			s[i] = s[i] - 32;
-		}
+
+	if (s[i] >= 97; && s[i] <= 122)
+	{
+		p[a] -= 32; 
+	}
+
+	i++
 	}
 
 	return (s);
