2b4b6729adf5860dad5560e0be21d5ed87514c39
Elizabeth K. G-S <81371428+lizodus@users.noreply.github.com>
2022-10-28 03:55:39 +0400

    still fixing

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,13 +10,13 @@
 char *string_toupper(char *s)
 {
 	int i;
+	i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*(s + i))
 	{
-		if (s[i] >= 'a' && s[i] <= 'z')
-		{
-			s[i] = s[i] - 32;
-		}
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+			*(s + i) -= 'a' - 'A';
+		i++;
 	}
 
 	return (s);
